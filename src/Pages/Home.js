e6061fd47bee19d977c21ad83b59d7f5eddc30a1
github username <github email address>
2019-07-19 12:55:51 -0700

    added background image and prelim text

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
+import back0 from '../Pictures/back0.jpg'
+import back1 from '../Pictures/back1.jpg'
+import { relative } from 'path';
+
+var images = {
+  0: `url(${back0})`,
+  1: `url(${back1})`
+}
+
+var sectionStyle = {
+  width: "100%",
+  height: "1100px",
+  backgroundImage: images[1],
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  position: 'relative'
+};
+
 export default function Home() {
 
     return (
-        <div>
-            Home
+        <div style={ sectionStyle } className='home'>
+          <div className='home-text-container'>
+            <div className='home-text'>
+              About text
+            </div>
+          </div>
         </div>
     )
 }
