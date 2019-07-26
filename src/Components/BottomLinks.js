21b7e4dfc7832364b82ea9756e9f087c2aee26d6
github username <github email address>
2019-07-26 15:10:31 -0700

    about content done, some spacing issues

---
+++ src/Components/BottomLinks.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function BottomLinks() {
+
+    return (
+        <div className="bottom-links">
+            <a href='https://www.linkedin.com/in/jon-langkammer/'>
+                <button className="ui circular linkedin icon button">
+                    <i className="linkedin icon"></i>
+                </button>
+            </a>
+            <a href='https://github.com/ConditionalStatementLifestyle'>
+                <button className="ui circular github icon button">
+                    <i className="github icon"></i>
+                </button>
+            </a>
+        </div>
+    )
+}
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
 
 import back0 from '../Pictures/back0.jpg'
 import back1 from '../Pictures/back1.jpg'
@@ -11,19 +10,19 @@ var images = {
 
 var sectionStyle = {
   width: "100%",
-  height: "1100px",
+  height: "1400px",
   backgroundImage: images[0],
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
-  position: 'absolute'
+//   position: 'absolute'
 };
 
 export default function About() {
 
     return (
         <div style={ sectionStyle } className='home'>
-            <div className='about'>
+            <div className='about-paragraph'>
                 Hi, my name is Jon and I'm a former electrical engineer with experience in the automation industry. 
                 After receiving my bachelor's degree in electrical engineering from the University of Minnesota, 
                 I took a job at a mid-size areospace supplier in Mukilteo, WA. During my tenure there, I was lucky 
@@ -31,7 +30,7 @@ export default function About() {
                 machine for the Boeing 777X. By far this was the most far reaching professional project I've ever been a part of, 
                 considering that every 777X wing will be touched by that machine. 
             </div>
-            <div className='about'>
+            <div className='about-paragraph'>
                 After about 2 years there, I felt a desire to write more code, so I found a job back in my home town
                 as a controls engineer in the industrial automation realm. Although I was able to write some machine code and 
                 work with touch screen software layouts, I realized that what I was really interested in was cutting edge, 
@@ -39,9 +38,16 @@ export default function About() {
                 or computers. Life is short, so I decided to enroll in Flatiron School's web development immersive program in Seattle
                 as a full time student. 
             </div>
-            <div className='about'>
-                Now I'm looking to combine my newly found coding skills with my professional engineering experiences in a position as 
+            <div className='about-paragraph'>
+                I am grateful for the experience I was able to be a part of at Flatiron as I had a ton of fun 
+                learning some of the relavent frontend and backend technologies in use today. Now that I've completed the course,
+                I'm looking to combine my newly found coding skills with my professional engineering experiences in a position as 
                 a software engineer. 
+            </div>
+            <div className='about-paragraph'>
+                Outside of my professional endeavors, I am an avid outdoors person who loves hiking in the Cascades or Olympics, snowboarding 
+                at Whistler or Steven's Pass, biking up some of Seattle's treacherous hills, playing tennis or table tennis, or playing frisbee. 
+                Besides that, I am a music lover and musician and I thoroughly enjoy playing N64 games with others or playing a nice round of Halo 2. 
             </div>
         </div>
     )
