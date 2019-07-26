dcac9ad87fa4f45353436ff50c342a3a6f1b6474
github username <github email address>
2019-07-26 12:52:12 -0700

    About page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Intro from './Pages/Intro'
 import Home from './Pages/Home'
+import About from './Pages/About'
 import Projects from './Pages/Projects'
 import NotFound from './Pages/NotFound'
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
@@ -17,6 +18,7 @@ function App() {
           <Route exact path='/' render={() => <Redirect to='/intro'/>}/>
           <Route exact path='/intro' render={() => <Intro/>}/>
           <Route exact path='/home' render={() => <Home/>}/>
+          <Route exact path='/about' render={() => <About/>}/>
           <Route exact path='/projects' render={() => <Projects/>}/>
           <Route exact path='/404' render={() => <NotFound />}/>
           <Route path='*' render={() => <Redirect to='/404'/>} />
