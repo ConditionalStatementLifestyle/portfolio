0b54eb2fa0d9dad7631715b53b42704c32a8acae
github username <github email address>
2019-07-27 12:35:54 -0700

    fixed modal props issue

--- src/Components/ProjectModal.js
+++ src/Components/ProjectModal.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import { Button, Header, Modal } from 'semantic-ui-react'
 
-export default function ProjectModal(props) {
+export default class ProjectModal extends React.Component {
 
-    return (
-        <div>
-            {/* <Button className='ui black button' onClick={() => this.props.toggleOpen()}>More Info</Button> */}
-            <Modal open={props.open} onClose={() => props.toggleOpen()}>
-            <Modal.Header>{props.projectInfo.name}</Modal.Header>
-            <Modal.Content image>
-                <Modal.Description>
-                <Header size='large'>{props.projectInfo.description}</Header>
-                <div>Frontend:{props.projectInfo.frontend}</div><br></br>
-                <div>Backend:{props.projectInfo.backend}</div><br></br>
-                <div>Technologies:{props.projectInfo.technologies}</div><br></br>
-                <div><a href={props.projectInfo.url}>URL</a></div><br></br>
-                </Modal.Description>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button color='black' onClick={() => props.toggleOpen()}>
-                Close
-                </Button><br></br><br></br>
-            </Modal.Actions>
-            </Modal>
-        </div>
-    )
+    constructor() {
+        super()
+        this.state = {
+            open : false
+        }
+    }
+
+    show = () => {
+        this.setState({ open: true })
+    }
+    
+    close = () => {
+        this.setState({ open : false })
+    }
+
+    render() {
+        return (
+            <div>
+                <Button className='ui black button' onClick={() => this.show()}>More Info</Button>
+                <Modal open={this.state.open} onClose={() => this.close}>
+                <Modal.Header>{this.props.projectInfo.name}</Modal.Header>
+                <Modal.Content image>
+                    <Modal.Description>
+                    <Header size='medium'>{this.props.projectInfo.description}</Header>
+                    <div>Frontend: {this.props.projectInfo.frontend}</div><br></br>
+                    <div>Backend: {this.props.projectInfo.backend}</div><br></br>
+                    <div>Technologies: {this.props.projectInfo.technologies}</div><br></br>
+                    <div><a href={this.props.projectInfo.url}>Go to demo</a></div><br></br>
+                    </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button color='black' onClick={() => this.close()}>
+                        Close
+                    </Button><br></br>
+                </Modal.Actions>
+                </Modal>
+            </div>
+        )
+    }
 }
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -21,7 +21,7 @@ var sectionStyle = {
 export default function About() {
 
     return (
-        <div style={ sectionStyle } className='home'>
+        <div style={ sectionStyle } className='about-background'>
             <div className='about-paragraph'>
                 Hi, my name is Jon and I'm a former electrical engineer with experience in the automation industry. 
                 After receiving my bachelor's degree in electrical engineering from the University of Minnesota, 
@@ -31,7 +31,7 @@ export default function About() {
                 considering that every 777X wing will be touched by that machine. 
             </div>
             <div className='about-paragraph'>
-                After about 2 years there, I felt a desire to write more code, so I found a job back in my home town
+                After about 2 years there, I felt a desire to write more code, so I found a job back in my home town (in Wisconsin)
                 as a controls engineer in the industrial automation realm. Although I was able to write some machine code and 
                 work with touch screen software layouts, I realized that what I was really interested in was cutting edge, 
                 user centric software, like the kind each of us use on a daily basis. I wanted to make applications for phones
@@ -47,7 +47,8 @@ export default function About() {
             <div className='about-paragraph'>
                 Outside of my professional endeavors, I am an avid outdoors person who loves hiking in the Cascades or Olympics, snowboarding 
                 at Whistler or Steven's Pass, biking up some of Seattle's treacherous hills, playing tennis or table tennis, or playing frisbee. 
-                Besides that, I am a music lover and musician and I thoroughly enjoy playing N64 games with others or playing a nice round of Halo 2. 
+                Besides that, I am a music lover/musician and I thoroughly enjoy playing N64 games with others or playing a nice round of Halo 2, in 
+                addition to being a hobbyist photographer. 
             </div>
         </div>
     )
